7f1beb82ff9f00c660412b4d7a79885b6aca705c
sathish007-msd <sathish.p2024cse@sece.ac.in>
2024-12-26 19:17:38 +0530

    changes done

---
+++ buynow.js
@@ -0,0 +1,6 @@
+document.getElementById('buyNowButton').addEventListener('click', function() {
+    alert('Thank you for your purchase!');
+    // Redirect to checkout page
+    window.location.href = 'checkout.html';
+});
+
